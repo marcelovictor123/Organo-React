2cb664f874bb6f806efd05de27a1681fb73fa7f8
marcelo_victor <marcelo.victor525@gmail.com>
2023-02-09 23:09:01 -0300

    Adicionando campo de  texto

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -1,6 +1,6 @@
 import './Banner.css'
 
-function Banner() {
+export const Banner = () => {
     return (
         <header className="banner">
             <img src="/imagens/banner.png" alt="Banner principal da pagina do organo" />
@@ -8,5 +8,3 @@ function Banner() {
     )
 
 }
-
-export default Banner
