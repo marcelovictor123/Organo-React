fe80b2ae0cda1502457a30937330b18c1c203a74
marcelo_victor <marcelo.victor525@gmail.com>
2023-02-16 23:48:32 -0300

    Criando evento no input

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,10 +1,17 @@
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
+
+    let valor = 'marcelo'
+
+    const aoDigitar = (evento) => {
+        valor = evento.target.value
+        console.log(valor)
+    }
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={props.placeholder} />
+            <input value={valor} required={props.obrigatorio} placeholder={props.placeholder} />
         </div>
 
     )
