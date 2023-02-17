06f88ee275393eaf97e4a6cd4eae97374fadb870
marcelo_victor <marcelo.victor525@gmail.com>
2023-02-17 15:01:21 -0300

    Controlando o estado

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,17 +1,17 @@
+
 import './CampoTexto.css'
 
 const CampoTexto = (props) => {
 
-    let valor = 'marcelo'
+
 
     const aoDigitar = (evento) => {
-        valor = evento.target.value
-        console.log(valor)
+        props.aoAlterado(evento.target.value)
     }
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input value={valor} required={props.obrigatorio} placeholder={props.placeholder} />
+            <input value={props.valor} onChange={aoDigitar} required={props.obrigatorio} placeholder={props.placeholder} />
         </div>
 
     )
